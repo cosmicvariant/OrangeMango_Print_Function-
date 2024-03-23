edd89bfc4014b405bf210e47c51123b810367906
Daniel Moabelo <85935686+cosmicvariant@users.noreply.github.com>
2024-03-23 17:41:15 +0200

    Add files via upload

---
+++ OrangeMango.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class OrangeMango
+{
+    static void Main(string[] args)
+    {
+        PrintNumbersWithConditions();
+    }
+
+    static void PrintNumbersWithConditions()
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("OrangeMango");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("Orange");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Mango");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
